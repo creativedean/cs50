02e178048f9ffb43d70cb465d1115da1af85a624
Jean <jeean9@gmail.com>
2023-12-31 01:55:54 +0000

    Create a Mario game in Scratch.
    This problem set challenges students to use their knowledge of Scratch to create a simple Mario game. Students must create a character that can move left and right, jump, and collect coins.

---
+++ mario/more/mario.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int height = 0;
+
+int main(void)
+{
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    int counter = 1;
+
+    while (counter <= height)
+    {
+        int space = height - counter;
+
+        for (int i = 0; i < space; i++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < counter; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+
+        for (int h = 0; h < counter; h++)
+        {
+            printf("#");
+        }
+        printf("\n");
+        counter++;
+    }
+}
